06ee7c525adbe477fb6bf9aeaa6f8f1f1017ee85
artem <artem.kucheravy@gmail.com>
2023-09-08 18:39:10 +0000

    Rewrite bot to be always online + separate into 2 files

--- scripts_bot/remBot/remBot.py
+++ scripts_bot/remBot/remBot.py
@@ -1,154 +1,8 @@
-import logging
-import asyncio
-import calendar
-import re
-from MySQLdb import _mysql
+from re import sub
+from remUtils import *
 from aiogram import Bot, Dispatcher, types, executor
-from remConstants import *
-from datetime import datetime, date, timedelta
-
-logging.basicConfig(level=logging.INFO)
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)   
-
-async def send_msg(to : int, msg : str):
-    # maximum length of a telegram message is 4096 symbols. if msg is too big:
-    # 1 - find last newline in given interval (from <i> to <i + max_len>)
-    # 2 - create chunk from <i> to <\n pos>
-    # 3 - <i> = <\n pos + 1>
-    # repeat until <i> is less than length of a message
-    msg_list = []
-    if len(msg) >= MAX_MESSAGE_LENGTH:
-        i = 0
-        while i < len(msg):
-            left = i
-            right = i + MAX_MESSAGE_LENGTH
-            line = msg[left:right]
-            # this regex searches for the first newline from right side of a string
-            # i got it from chatgpt, because both rfind and find for some reason returned -1 even though
-            # in operator returned True... Super weird, but this works
-            match = re.search(r"\n(?=[^\n]*$)", line)
-            if match:
-                n = match.start() + left # match is in line but i need to get from whole msg
-            else:
-                n = -1
-            if n != -1:
-                line = msg[left:n+1]
-                i = n + 1
-            msg_list.append(line)
-    else:
-        msg_list.append(msg)
-    for chunk in msg_list:
-        #print(chunk)
-        if not(chunk == "" or chunk == '\n'):
-            #print("mrdown mode = ", mode)
-            await bot.send_message(to, chunk)
-
-async def check_usr(from_id : int, message : types.Message):
-    if not from_id in users:
-        await message.reply("Sorry, this is a family bot, strangers are not allowed.\n\nHave a nice day!")
-        return False
-    return True
-        
-
-async def is_valid_date(date_string):
-    try:
-        # Attempt to parse the date string using datetime.strptime
-        datetime.strptime(date_string, '%Y-%m-%d')
-        return True
-    except ValueError:
-        return False
-
-async def add_months(sourcedate, months):
-    month = sourcedate.month - 1 + months
-    year = sourcedate.year + month // 12
-    month = month % 12 + 1
-    day = min(sourcedate.day, calendar.monthrange(year,month)[1])
-    return date(year, month, day)
-
-async def get_query(query: str):
-    db = _mysql.connect(host="localhost", user=usr,
-                        password=pswd, database=dbase)
-    db.query(query)
-    query_res = db.store_result()
-    query_res = query_res.fetch_row(maxrows=0, how=1)
-    i = 0
-    list_res = []
-    for query_row in query_res:
-        for column in query_row:
-            try:
-                query_row[column] = str(query_row[column], "utf-8")
-            except TypeError:
-                query_row[column] = None
-        list_res.append(query_row)
-    db.close()
-    return list_res
-
-async def insert_data(queries: list):
-    db = _mysql.connect(host="localhost", user=usr,
-                        password=pswd, database=dbase)
-    for query in queries:
-        db.query(query)
-    db.commit()  # commit the changes made to the database
-    db.close()
-
-async def get_today():
-    # was needed for tests mainly
-    #return datetime.strptime("2023-07-09", "%Y-%m-%d").date()
-    return date.today()
-    
-async def reschedule(day : dict) -> None:
-    if day["period"] == None or int(day["period_am"]) == None:
-        # if these fields are not provided event does not need to be rescheduled
-        await insert_data([f"DELETE FROM DAYS WHERE id = {day['id']}"])
-        return
-    vDate = datetime.strptime(day['day'], "%Y-%m-%d").date()
-    period = day["period"]
-    amount = int(day["period_am"])
-    if period == "year":
-        try:
-            vDate = vDate.replace(year = vDate.year + amount)
-        except ValueError:
-            # exception for 'visokosniy' year (29 february transfers to 1 march)
-            vDate = vDate + (date(vDate.year + amount, 1, 1) - date(vDate.year, 1, 1))
-    elif period == "week":
-        vDate = vDate + timedelta(days=7)
-    elif period == "day":
-        vDate = vDate + timedelta(days=1)
-    elif period == "month":
-        vDate = await add_months(vDate, 1)   
-    querris = []
-    querris.append(f'UPDATE DAYS SET day = DATE("{vDate.strftime("%Y-%m-%d")}") WHERE id = {day["id"]}')
-    await insert_data(querris)
-
-async def check_day(day : dict) -> bool:
-    # actually i also need to adjust timezones
-    # but i dont care
-    # return "" if no need to remind or string with reminder itself
-    date = datetime.strptime(day['day'], "%Y-%m-%d").date()
-    today = await get_today()
-    tomorrow = today + timedelta(days=1)
-    week = today + timedelta(days=7)
-    if date == tomorrow:
-        return f"Attention!\n\nTomorrow is {day['descr']}"
-    elif date == week:
-        return f"Attention!\n\nIn 7 days there is {day['descr']}"
-    elif date == today:
-        await reschedule(day)
-        return f"Attention!\n\nToday is {day['descr']}"
-    else:
-        return ""
-
-async def remind():
-    days = await get_query("SELECT * FROM DAYS ORDER BY day")
-    for day in days:
-        answ = await check_day(day)
-        if answ > "":
-            await bot.send_message(chat, answ)    
-
-async def close():
-    session = await bot.get_session()
-    await session.close()
+from remUtils import *
+from datetime import date
 
 @dp.message_handler(commands=['print'])
 async def get_menu(message: types.Message):
@@ -157,19 +11,27 @@ async def get_menu(message: types.Message):
     days = await get_query("SELECT * FROM DAYS ORDER BY day")
     msg = f"There are {len(days)} events:\n\n"
     for day in days:
-        row = f"{day['day']}: {day['descr']}."
+        row = "* " + f"{day['day']}: {day['descr']}."
         if day['period_am'] != None and day['period'] != None:
-            row = row + f" Repeat after {day['period_am']} {day['period']}."
+            row = row + f" Repeat every {day['period_am']} {day['period']}."
         else:
             row = row + f" Don`t repeat."
         row = row + f" (id:{day['id']})"
-        msg = msg + row + "\n"
+        msg = msg + row + "\n\n"
     await send_msg(to=message.from_user.id, msg=msg)
+
 
 @dp.message_handler(commands=['add'])
 async def add_event(message: types.Message):
     if not await check_usr(message.from_user.id, message):
         return    
+    if message.text.count('"') > 2:
+        await send_msg(to=message.from_user.id, msg="Error: You should only use double quotes to enquote event description. Otherwise I can`t work 😭")
+        return
+    elif message.text.count('"') < 2:
+        await send_msg(to=message.from_user.id, msg="Error: You must use double quotes to enquote event description. Otherwise I can`t work 😭")
+        return
+    message.text = sub(r'"(.*?)"', replace_spaces, message.text)
     # date description period(optional) period_am(optional)
     args = message.get_args().split()
     if len(args) != 2 and len(args) != 4:
@@ -190,6 +52,7 @@ async def add_event(message: types.Message):
         except ValueError:
             await send_msg(to=message.from_user.id, msg=f"Period amount should be an integer.")
             return
+    args[1] = args[1].replace(delim, ' ')
     query = "INSERT INTO DAYS (day, descr"
     if len(args) == 2:
         query = f"INSERT INTO DAYS (day, descr) VALUES('{args[0]}', '{args[1]}')"
@@ -223,13 +86,18 @@ async def add_event(message: types.Message):
 async def add_event(message: types.Message):
     if not await check_usr(message.from_user.id, message):
         return
-    msg = "I can answer to following commands:"
+    msg = "I can answer to following 4 commands:"
     msg = msg + "\n" + "1) /check - If a message is written, then I am working, if not then I`m down."
-    msg = msg + "\n" + "2) /prints - Prints out information about all events in a database."
-    msg = msg + "\n" + "3) /delete id - id is an integer which uniquely identifies an event in a database. Deleted event can not be recovered."
-    msg = msg + "\n" + "4) /add date description period period_amount - Schedules a new event in a database. date is the events date, description - long text about event"
-    msg = msg + ", period (year,month,week,day) is needed if you want event to be repeated after this period, period_amount - how many 'periods' should pass until event is repeated"
-    msg = msg + " period and period_amount can be omitted"
+    msg = msg + "\n" + "2) /print - Prints out information about all events in a database."
+    msg = msg + "\n" + "3) /delete <id> - <id> is an integer which uniquely identifies an event in a database. Deleted event can not be recovered. You can obtains events id from /print command."
+    msg = msg + "For example, if I want to delete event with id 777, I have to write following command:"
+    msg = msg + '\n\n' + "/delete 777"
+    msg = msg + "\n\n" + '4) /add <date> <"description of an event"> <period> <period_amount> - Schedules a new event in a database. <date> is the events date (should be in yyyy-mm-dd format), <description> - long text about event'
+    msg = msg + ", <period> (year,month,week,day) is needed if you want event to be repeated after this period, <period_amount> - how many 'periods' should pass until event is repeated"
+    msg = msg + " <period> and <period_amount> can be omitted. For example, if I want to schedule event called 'Giga day' on 22 april 2023 and it happens every year, I must write following:"
+    msg = msg + '\n\n' + '/add 2023-04-22 "Giga day" year 1' + '\n\n' + 'But if this event happens only once, I must write:'
+    msg = msg + '\n\n' + '/add 2023-04-22 "Giga day"'
+    msg = msg + '\n\n' + 'By the way, I dont have implemented "edit" button, so to change an event, simply delete the old one and create another one with correct information 😉'
     await send_msg(to=message.from_user.id, msg=msg)
 
 @dp.message_handler(commands=['check'])
@@ -238,9 +106,17 @@ async def handle_check_command(message: types.Message):
         return
     await message.reply("Alive!" + " " + str(message.from_user.id))
 
+async def remind(bot : Bot, reschedule : bool):
+    days = await get_query("SELECT * FROM DAYS ORDER BY day")
+    for day in days:
+        answ = await check_day(day, reschedule)
+        if answ > "":
+            await bot.send_message(chat, answ)    
+
+async def on_startup(dp : Dispatcher):
+    scheduler.add_job(remind, 'cron', hour='8', minute='00', args=(bot, False,))
+    scheduler.add_job(remind, 'cron', hour='18', minute='00', args=(bot, True,))
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
-    #loop = asyncio.new_event_loop()
-    #asyncio.set_event_loop(loop)
-    #loop.run_until_complete(remind())
-    #loop.run_until_complete(close())
+    scheduler.start()
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
---
+++ scripts_bot/remBot/remUtils.py
@@ -0,0 +1,143 @@
+import logging
+import calendar
+import re
+
+from MySQLdb import _mysql
+from aiogram import Bot, Dispatcher, types
+from remConstants import *
+from datetime import datetime, date, timedelta
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=TOKEN)
+scheduler = AsyncIOScheduler()
+dp = Dispatcher(bot)
+
+async def send_msg(to : int, msg : str):
+    # maximum length of a telegram message is 4096 symbols. if msg is too big:
+    # 1 - find last newline in given interval (from <i> to <i + max_len>)
+    # 2 - create chunk from <i> to <\n pos>
+    # 3 - <i> = <\n pos + 1>
+    # repeat until <i> is less than length of a message
+    msg_list = []
+    if len(msg) >= MAX_MESSAGE_LENGTH:
+        i = 0
+        while i < len(msg):
+            left = i
+            right = i + MAX_MESSAGE_LENGTH
+            line = msg[left:right]
+            # this regex searches for the first newline from right side of a string
+            # i got it from chatgpt, because both rfind and find for some reason returned -1 even though
+            # in operator returned True... Super weird, but this works
+            match = re.search(r"\n(?=[^\n]*$)", line)
+            if match:
+                n = match.start() + left # match is in line but i need to get from whole msg
+            else:
+                n = -1
+            if n != -1:
+                line = msg[left:n+1]
+                i = n + 1
+            msg_list.append(line)
+    else:
+        msg_list.append(msg)
+    for chunk in msg_list:
+        if not(chunk == "" or chunk == '\n'):
+            await bot.send_message(to, chunk)
+
+async def check_usr(from_id : int, message : types.Message):
+    if not from_id in users:
+        await message.reply("Sorry, this is a family bot, strangers are not allowed.\n\nHave a nice day!")
+        return False
+    return True
+
+async def is_valid_date(date_string):
+    try:
+        # Attempt to parse the date string using datetime.strptime
+        datetime.strptime(date_string, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
+
+async def add_months(sourcedate, months):
+    month = sourcedate.month - 1 + months
+    year = sourcedate.year + month // 12
+    month = month % 12 + 1
+    day = min(sourcedate.day, calendar.monthrange(year,month)[1])
+    return date(year, month, day)
+
+async def get_query(query: str):
+    db = _mysql.connect(host="localhost", user=usr,
+                        password=pswd, database=dbase)
+    db.query(query)
+    query_res = db.store_result()
+    query_res = query_res.fetch_row(maxrows=0, how=1)
+    i = 0
+    list_res = []
+    for query_row in query_res:
+        for column in query_row:
+            try:
+                query_row[column] = str(query_row[column], "utf-8")
+            except TypeError:
+                query_row[column] = None
+        list_res.append(query_row)
+    db.close()
+    return list_res
+
+async def insert_data(queries: list):
+    db = _mysql.connect(host="localhost", user=usr,
+                        password=pswd, database=dbase)
+    for query in queries:
+        db.query(query)
+    db.commit()  # commit the changes made to the database
+    db.close()
+
+async def get_today():
+    # was needed for tests mainly
+    #return datetime.strptime("2023-07-09", "%Y-%m-%d").date()
+    return date.today()
+    
+async def reschedule(day : dict) -> None:
+    if day["period"] == None or int(day["period_am"]) == None:
+        # if these fields are not provided event does not need to be rescheduled
+        await insert_data([f"DELETE FROM DAYS WHERE id = {day['id']}"])
+        return
+    vDate = datetime.strptime(day['day'], "%Y-%m-%d").date()
+    period = day["period"]
+    amount = int(day["period_am"])
+    if period == "year":
+        try:
+            vDate = vDate.replace(year = vDate.year + amount)
+        except ValueError:
+            # exception for 'visokosniy' year (29 february transfers to 1 march)
+            vDate = vDate + (date(vDate.year + amount, 1, 1) - date(vDate.year, 1, 1))
+    elif period == "week":
+        vDate = vDate + timedelta(days=7)
+    elif period == "day":
+        vDate = vDate + timedelta(days=1)
+    elif period == "month":
+        vDate = await add_months(vDate, 1)   
+    querris = []
+    querris.append(f'UPDATE DAYS SET day = DATE("{vDate.strftime("%Y-%m-%d")}") WHERE id = {day["id"]}')
+    await insert_data(querris)
+
+async def check_day(day : dict, reschedule : bool) -> str:
+    # actually i also need to adjust timezones
+    # but i dont care
+    # return "" if no need to remind or string with reminder itself
+    date = datetime.strptime(day['day'], "%Y-%m-%d").date()
+    today = await get_today()
+    tomorrow = today + timedelta(days=1)
+    week = today + timedelta(days=7)
+    if date == tomorrow:
+        return f"Attention!\n\nTomorrow is {day['descr']}"
+    elif date == week:
+        return f"Attention!\n\nIn 7 days there is {day['descr']}"
+    elif date == today:
+        if reschedule:
+            await reschedule(day)
+        return f"Attention!\n\nToday is {day['descr']}"
+    else:
+        return ""
+    
+def replace_spaces(match):
+    return match.group(0).replace(' ', delim)
